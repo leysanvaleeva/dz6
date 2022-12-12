83adaced62e0b5d0d6c08a418364d6f6df29c1c9
leysan <leis90@list.ru>
2022-12-12 23:49:20 +0300

    3

---
+++ z1/Program.cs
@@ -0,0 +1,37 @@
+﻿// // задача 1 - HARD необязательная. На вход программы подаются одно целое число. Составьте список чисел Фибоначчи,
+// //  в том числе для отрицательных индексов. Это по сути последовательность Негафибоначчи.
+
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+// int size = n * 2 + 1;
+
+// Fibonacci(n);
+NegaFibonacci(n);
+
+// Числа Фибонначи
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+// double Fibonacci(int n)
+// {
+//     if (n == 1 || n == 2) return 1;
+//     else return Fibonacci(n - 1) + Fibonacci(n - 2);
+// }
+// for (int i = 1; i <= n; i++)
+// {
+//     Console.Write($"{Fibonacci(i)} ");
+// }
+
+double NegaFibonacci(int n)
+{
+    if (n == -1) return 1;
+    else if (n == -2) return -1;
+    else return NegaFibonacci(n + 2) - NegaFibonacci(n + 1);
+}
+for (int j = -n; j <= -1; j++)
+{
+    Console.Write($"{NegaFibonacci(j)} ");
+}
+
---
+++ zad41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("Введите количество цифр, которые будут использованы в этой программе."); 
+int size = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите целыe числа, через 'enter'."); 
+int[] array = new int [size]; 
+for (int i=0; i<size; i++) 
+array[i]=Convert.ToInt32(Console.ReadLine()); 
+PrintArray(array); 
+Count (array);
+ 
+void PrintArray(int[] array) 
+{ 
+    foreach (int el in array)   
+        Console.Write($"{el} "); 
+        Console.WriteLine(); 
+}
+
+void Count (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]>0) count+=1;
+    }
+    System.Console.WriteLine($"Пользователь ввел {count} чисел больше 0.");
+}
